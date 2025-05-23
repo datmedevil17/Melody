7a74458f4ce9a110184ad0fdc6f6e43e9414df69
datmedevil17 <rakshitshukla17@gmail.com>
2025-05-23 19:04:39 +0530

    corrected homepage

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,11 @@
 import LandingPage from "@/components/landingPage";
-
+import HomePage2 from "@/components/HomePage2";
 export default function Home() {
   return (
     <div>
-      < LandingPage />
+      
+       <HomePage2 /> 
+      
     </div>
   );
 }
