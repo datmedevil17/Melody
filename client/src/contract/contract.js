e80123c73098c9e1c1514804117d8dde9978619e
datmedevil17 <rakshitshukla17@gmail.com>
2025-05-24 03:32:23 +0530

    resolved contracts

--- client/src/contract/contract.js
+++ client/src/contract/contract.js
@@ -1,7 +1,7 @@
-export const artistContractAddress = '0x067dd9e465a7e30413c7e9d9ecc4cb5d1e4d177e1dc6ebe2313b1b077f033781'
+export const artistContractAddress = '0x07e7d7a83258c1345b76aef0e7d78bcb570214602a18e2470525d5f65f5959ed'
 export const tokenContractAddress = '0x04bcc2528cc6a9acec235c28882f7b8cd1c57e775ff8a9c16fb023130158da71'
-export const songContractAddress = '0x05c1db326d59c86d00bccb1ee80b63d8141534a5cfb99a9a367e97155b3f4d84'
-export const userContractAddress = '0x041834b7d9eda1d2e69a894f5504a6d89dc632659c4bbd5cf9835e7f48ed2014'
+export const songContractAddress = '0x027c7aa55d9c9330f2c3f34f7a9a9a9063fbf00509e309a5f90692ba72758c6e'
+export const userContractAddress = '0x0489564bdcec3671525865c712b207625f17fc48455a548bfa1daf686fff1bd4'
 export const tippingContractAddress = '0x049c673d3cae8c7562f3b7ae8ac82eeff9ea19f4e64ed515f5884ad779749a99'
 
 
